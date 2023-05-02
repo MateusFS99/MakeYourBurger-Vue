3951784e0c8b506038ba832ced1517c2270c5c94
Mateus Fonseca Santos <mateusfsantos.pfs@gmail.com>
2023-05-02 01:35:31 -0300

    - Feat: Burger Form Created

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,15 +2,18 @@
   <BannerComponent />
   <div class="main-container">
     <h1>Monte o seu burger:</h1>
+    <BurgerFormComponent />
   </div>
 </template>
 
 <script>
 import BannerComponent from "../components/BannerComponent.vue";
+import BurgerFormComponent from "../components/BurgerFormComponent.vue";
 
 export default {
   components: {
     BannerComponent,
+    BurgerFormComponent
   },
 }
 </script>
